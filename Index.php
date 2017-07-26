ef7c58a6d8e1391e7da8a2a2ceaf1fd8850ad9e9
KalyaniBhade <bhade.kalyani16@gmail.com>
2017-07-26 15:51:39 +0530

    Added files

---
+++ Index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+
+</head>
+<body>
+<form action="Result.php" method="post">
+    Name: <input type="text" name="name">
+    <br/>
+    E-mail: <input type="text" name="email">
+    <br/>
+
+    Gender:
+    <input type="radio" name="gender"
+           value="female">Female</form>
+    <input type="radio" name="gender"
+       value="male">Male</body><br/>
+     <input type="button" name="Submit" value="Submit">
+</form>
+</body>
+</html>
---
+++ Result.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kalyani
+ * Date: 26-Jul-17
+ * Time: 3:26 PM
+ */
