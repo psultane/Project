7f5dc5707a415a8635b22017d323e7a596280cee
KalyaniBhade <bhade.kalyani16@gmail.com>
2017-07-27 14:05:10 +0530

    Changed

--- Index.php
+++ Index.php
@@ -8,12 +8,6 @@
     <br/>
     E-mail: <input type="text" name="email">
     <br/>
-
-    Gender:
-    <input type="radio" name="gender"
-           value="female">Female</form>
-    <input type="radio" name="gender"
-       value="male">Male</body><br/>
      <input type="button" name="Submit" value="Submit">
 </form>
 </body>
