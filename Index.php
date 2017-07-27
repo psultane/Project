efd1540ce7436f5670afa8d3eada38a5819ed8b0
KalyaniBhade <bhade.kalyani16@gmail.com>
2017-07-27 14:17:14 +0530

    Hello

--- Index.php
+++ Index.php
@@ -6,7 +6,7 @@
 <form action="Result.php" method="post">
     Name: <input type="text" name="name">
     <br/>
-    E-mail: <input type="text" name="email">
+    E-mail id: <input type="text" name="email">
     <br/>
      <input type="button" name="Submit" value="Submit">
 </form>
